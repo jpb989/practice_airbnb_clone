9746b01a947a4f9bf56349737fc9f18775dad10f
jpb989 <77682780+jpb989@users.noreply.github.com>
2024-09-30 18:07:27 +0000

    Converted submodule 'frontend' to a regular folder

---
+++ frontend/app/components/Categories.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image"
+
+const Categories = () => {
+  return (
+    <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+          <Image
+            src="/icn_views.jpg"
+            alt="Amazing views"
+            width={25}
+            height={25}
+          />
+          <span className="text-xs">Amazing Views</span>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+          <Image
+            src="/icn_pools.jpg"
+            alt="Amazing pools"
+            width={25}
+            height={25}
+          />
+          <span className="text-xs">Amazing Pools</span>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+          <Image
+            src="/icn_farms.jpg"
+            alt="Farms"
+            width={25}
+            height={25}
+          />
+          <span className="text-xs">Farms</span>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+          <Image
+            src="/icn_countryside.jpg"
+            alt="Countryside"
+            width={25}
+            height={25}
+          />
+          <span className="text-xs">Countryside</span>
+        </div>
+    </div>
+  )
+}
+
+export default Categories
---
+++ frontend/app/components/properties/ReservationSidebar.tsx
@@ -0,0 +1,11 @@
+
+const ReservationSidebar = () => {
+  return (
+    <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
+        <h2 className="mb-5 text-2xl">$200 per night</h2>
+
+    </aside>
+  )
+}
+
+export default ReservationSidebar

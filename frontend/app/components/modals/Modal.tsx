6c3da55d57e92c625260b55a31aa619860a91cd0
jpb989 <joshpaulbakugaan@gmail.com>
2024-10-02 05:14:38 +0530

    modifying nav

---
+++ frontend/app/components/modals/Modal.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+interface ModalProps{
+
+}
+
+const Modal: React.FC<ModalProps> = () => {
+  return (
+    <div>Modal</div>
+  )
+}
+
+export default Modal

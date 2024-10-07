dbcf57e7663a5b4884ad81c82cd0f92ee79a7b2a
jpb989 <joshpaulbakugaan@gmail.com>
2024-10-08 02:44:09 +0530

    windoows local changes for fetching properties from backend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+            protocol: "http",
+            hostname: "localhost",
+            port: "8000",
+            pathname: "/**"
+        }]
+    }
+};
 
 export default nextConfig;
